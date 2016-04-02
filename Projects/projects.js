18ab0830807b233a12b674f102bf12a0d2693098
jefrose <jefrose@paypal.com>
2016-04-01 20:13:16 -0500

    normalize style across places

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,27 +1,78 @@
-var ides = document.getElementById("ides");
+//xio
+var xio = document.getElementById("xio");
 
-ides.onmouseover = function () {
-    ides.style.color = '#3C729D';
+xio.onmouseover = function () {
+    xio.style.color = '#3C729D';
 };
 
-ides.onmouseout = function () {
-    ides.style.color = 'black'
+xio.onmouseout = function () {
+    xio.style.color = 'black'
 };
 
-ides.addEventListener('click', function () {
-    window.location.href = 'IDES.html';
+xio.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/xio';
 });
 
-var forgot2code = document.getElementById("forgot2code");
 
-forgot2code.onmouseover = function () {
-    forgot2code.style.color = '#3C729D';
+//xio2
+var xio2 = document.getElementById("xio2");
+
+xio2.onmouseover = function () {
+    xio2.style.color = '#3C729D';
 };
 
-forgot2code.onmouseout = function () {
-    forgot2code.style.color = 'black'
+xio2.onmouseout = function () {
+    xio2.style.color = 'black'
 };
 
-forgot2code.addEventListener('click', function () {
-    window.location.href = 'forgot2code.html';
+xio2.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/xio2';
+});
+
+
+// dots
+var dots = document.getElementById("dots");
+
+dots.onmouseover = function () {
+    dots.style.color = '#3C729D';
+};
+
+dots.onmouseout = function () {
+    dots.style.color = 'black'
+};
+
+dots.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/dotfiles';
+});
+
+
+// vim
+var vim = document.getElementById("vim");
+
+vim.onmouseover = function () {
+    vim.style.color = '#3C729D';
+};
+
+vim.onmouseout = function () {
+    vim.style.color = 'black'
+};
+
+vim.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/vim';
+});
+
+
+// emacs
+var emacs = document.getElementById("emacs");
+
+emacs.onmouseover = function () {
+    emacs.style.color = '#3C729D';
+};
+
+emacs.onmouseout = function () {
+    emacs.style.color = 'black'
+};
+
+emacs.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/emacs.d';
 });
