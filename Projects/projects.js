7cd0a3c9c1b17ac2810a1bdf0b19d0ef5d9befce
jefrose <jefrose@paypal.com>
2016-08-30 03:40:31 -0500

    lets start updating this again

--- Projects/projects.js
+++ Projects/projects.js
@@ -15,69 +15,19 @@ xio.addEventListener('click', function () {
 });
 
 
-//xio2
-var xio2 = document.getElementById("xio2");
+//chi
+var chi = document.getElementById("chi");
 
-xio2.onmouseover = function () {
-    xio2.style.color = '#3C729D';
-    xio2.style.cursor = 'pointer';
+chi.onmouseover = function () {
+    chi.style.color = '#3C729D';
+    chi.style.cursor = 'pointer';
 };
 
-xio2.onmouseout = function () {
-    xio2.style.color = 'black'
+chi.onmouseout = function () {
+    chi.style.color = 'black'
 };
 
-xio2.addEventListener('click', function () {
-    window.location.href = 'https://github.com/xjdr/xio2';
+chi.addEventListener('click', function () {
+    window.location.href = 'https://github.com/xjdr/chicago';
 });
 
-
-// dots
-var dots = document.getElementById("dots");
-
-dots.onmouseover = function () {
-    dots.style.color = '#3C729D';
-    dots.style.cursor = 'pointer';
-};
-
-dots.onmouseout = function () {
-    dots.style.color = 'black'
-};
-
-dots.addEventListener('click', function () {
-    window.location.href = 'https://github.com/xjdr/dotfiles';
-});
-
-
-// vim
-var vim = document.getElementById("vim");
-
-vim.onmouseover = function () {
-    vim.style.color = '#3C729D';
-    vim.style.cursor = 'pointer';
-};
-
-vim.onmouseout = function () {
-    vim.style.color = 'black'
-};
-
-vim.addEventListener('click', function () {
-    window.location.href = 'https://github.com/xjdr/vim';
-});
-
-
-// emacs
-var emacs = document.getElementById("emacs");
-
-emacs.onmouseover = function () {
-    emacs.style.color = '#3C729D';
-    emacs.style.cursor = 'pointer';
-};
-
-emacs.onmouseout = function () {
-    emacs.style.color = 'black'
-};
-
-emacs.addEventListener('click', function () {
-    window.location.href = 'https://github.com/xjdr/emacs.d';
-});
